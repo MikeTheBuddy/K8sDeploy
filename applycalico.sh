2d2beb123de73b6023d635d50faf87757b92d93a
MikeTheBuddy <michael.krivobarski@gmail.com>
2024-11-25 11:31:43 -0600

    Forgot to remove original files out from previous location

--- applycalico.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-helm repo add projectcalico https://docs.tigera.io/calico/charts
-
-kubectl create namespace tigera-operator
-
-helm install calico projectcalico/tigera-operator --version v3.29.0 --namespace tigera-operator
-
