5a72d0e3d54d695749e533a2b3398571093a0afd
MikeTheBuddy <michael.krivobarski@gmail.com>
2024-11-27 02:46:52 -0600

    Updated text and added Calico installation script

--- k8sdeploy.sh
+++ k8sdeploy.sh
@@ -126,7 +126,7 @@ if [ "$1" == "master" ]; then
   clear
 
   echo "Installation Finished!"
-  echo "Please initalize a cluster with the follow command... [sudo kubeadm init --pod-network-cidr=10.244.0.0/16]"
+  echo "Please initalize a cluster with the follow command... [sudo kubeadm init --pod-network-cidr=192.168.0.0/16]"
 
 
 else
